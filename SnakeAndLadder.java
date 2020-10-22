b22053f0fcb0ca1ef5bc316c8ec788910c8264bb
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-10-22 07:43:23 +0530

    Added : Option method added to check whether no play,ladder,snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,36 @@
 class Player{
-	int position=0;
+        int position=0;
+        int dieRoll=0;
+
         public int roll(){
-        int dieNumber=(int)((Math.random()*10)%6+1);
+        int dieNumber= (int)((Math.random()*10)%6+1);
         System.out.println(dieNumber);
-	return 0;
+        dieRoll++;
+        switch (option())
+        {
+                case 0:
+                        System.out.println("No Play, Current Position: " + position);
+                        return 0;
+                case 1:
+                        System.out.println("Ladder, Initial Position: " + position + ", Die Rolled: " + dieNumber + ", Final Position: " + (position + dieNumber));
+                        position+=dieNumber;
+                        return dieNumber;
+                default:
+                        System.out.println("Snake, Initial Position: " + position + ", Die Rolled: " + dieNumber + ", Final Position: " + (position - dieNumber));
+                        position-=dieNumber;
+                        return -dieNumber;
+        }
+        }
+
+        public int option(){
+        int action=(int)((Math.random()*10)%3);
+        return action;
         }
 }
 public class SnakeAndLadder{
         public static void main(String[] args){
         Player p=new Player();
         p.roll();
+        p.option();
         }
 }
