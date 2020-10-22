f8b428b8ba0aeda425ff987b66d79a16dd531d21
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-10-22 08:04:01 +0530

    Added : Condition added to ensure the exact winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -13,16 +13,27 @@ class Player{
                         System.out.println("No Play, Current Position: " + position);
                         return 0;
                 case 1:
+                                //Ensuring exact winning position
+                        if(position+dieNumber>100)
+                         {
+                                System.out.println("Ladder (Player exceeded 100), Current Position: " + position + ", Die Rolled: " + dieNumber);
+                                return dieNumber;
+                        }
                         System.out.println("Ladder, Initial Position: " + position + ", Die Rolled: " + dieNumber + ", Final Position: " + (position + dieNumber));
                         position+=dieNumber;
-                        return +dieNumber;
+                        return dieNumber;
                 default:
+                        if(position-dieNumber<0)
+                        {
+                                System.out.println("Snake (Player slumped below 0), Current Position: " + position + ", Die Rolled: " + dieNumber + ", Final Position: " + 0);
+                                position=0;
+                                  return -dieNumber;
+                        }
                         System.out.println("Snake, Initial Position: " + position + ", Die Rolled: " + dieNumber + ", Final Position: " + (position - dieNumber));
                         position-=dieNumber;
                         return -dieNumber;
         }
         }
-
         public int option(){
         int action=(int)((Math.random()*10)%3);
         return action;
@@ -33,7 +44,7 @@ public class SnakeAndLadder{
         Player p=new Player();
         p.roll();
         p.option();
-        while(p.position!=100){
+	      while(p.position!=100){
                 int dieRoll = -1;
                 dieRoll = p.roll();
                 if(dieRoll > 0){
