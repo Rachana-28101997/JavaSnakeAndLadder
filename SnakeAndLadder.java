93b4282543391b2d2c4a11d5b26a5a328ce77eda
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-10-22 07:13:39 +0530

    Added : Player class and position added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,5 +1,11 @@
+class Player{
+        int position=0;
+}
+
 public class SnakeAndLadder{
         public static void main(String[] args){
 	System.out.println("WELCOME TO SNAE AND LADDER PROBLEM");
-	}
+        Player p=new Player();
+
+        }
 }
